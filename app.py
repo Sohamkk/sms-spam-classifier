377c39fda8e7c5b375a93a1fede9a6559f340f43
Soham Kulkarni <102247588+Sohamkk@users.noreply.github.com>
2025-04-22 22:24:58 +0530

    Initial commit

--- app.py
+++ app.py
@@ -4,6 +4,13 @@ import string
 from nltk.corpus import stopwords
 import nltk
 from nltk.stem.porter import PorterStemmer
+import streamlit as st
+import pickle
+import string
+from nltk.corpus import stopwords
+import nltk
+from nltk.stem.porter import PorterStemmer
+
 
 ps = PorterStemmer()
 
